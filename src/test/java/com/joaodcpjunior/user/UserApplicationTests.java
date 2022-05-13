ecba1e5dc44d67b6db6edd8a91136daecfed014b
João de Deus <jdjunior2@stefanini.com>
2022-05-13 14:12:37 -0300

    bootstrap project

---
+++ src/test/java/com/joaodcpjunior/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaodcpjunior.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
