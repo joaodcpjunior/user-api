fa83002feca265f27ede5f908352ba8c869e9188
João de Deus <jdjunior2@stefanini.com>
2022-05-14 23:55:24 -0300

    implement resource layer

---
+++ src/main/java/com/joaodcpjunior/user/resource/UserResource.java
@@ -0,0 +1,74 @@
+package com.joaodcpjunior.user.resource;
+
+import com.joaodcpjunior.user.model.Response;
+import com.joaodcpjunior.user.model.User;
+import com.joaodcpjunior.user.service.implementation.UserServiceImplementation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+import static java.time.LocalDateTime.now;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserResource {
+    private final UserServiceImplementation userService;
+
+    @GetMapping
+    public ResponseEntity<Response> getUsers() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("users", userService.list()))
+                        .message("Users retrieved")
+                        .httpStatus(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<Response> saveUser(@RequestBody @Valid User user) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("user", userService.create(user)))
+                        .message("User created")
+                        .httpStatus(CREATED)
+                        .statusCode(CREATED.value())
+                        .build()
+        );
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("users", userService.get(id)))
+                        .message("User retrieved")
+                        .httpStatus(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> deleteUser(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(Map.of("deleted", userService.delete(id)))
+                        .message("User deleted")
+                        .httpStatus(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+}
